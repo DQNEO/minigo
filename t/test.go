f13574eca865bd457bf6e3aeeef135e15bbb84a5
DQNEO <dqneoo@gmail.com>
2018-10-08 23:36:44 +0900

    separate func

--- t/test.go
+++ t/test.go
@@ -1,8 +1,12 @@
 package main
 
-func main() {
-	puts("hello world")
+func f() {
 	printf("%d", 3 - 2)
 	printf("%d", 1 + 1)
 	printf("%d\n", 1 * 3)
+}
+
+func main() {
+	puts("hello world")
+	f()
 }
