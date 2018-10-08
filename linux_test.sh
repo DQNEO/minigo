03023d01c21a7ed3e6894b26072bad99056a05c3
DQNEO <dqneoo@gmail.com>
2018-10-08 16:35:00 +0900

    test by test.go

--- linux_test.sh
+++ linux_test.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 set -ex
-export PATH="/usr/lib/go-1.10/bin:$PATH"
+PATH="/usr/lib/go-1.10/bin:$PATH"
 
 as_file="./out/a.s"
+
+prog_name="minigo"
+go build -o minigo *.go
+
+function run_test_go {
+    ./${prog_name} < test/test.go > $as_file
+    gcc -no-pie -o out/a.out $as_file
+    ./out/a.out > out/actual.txt
+    diff out/actual.txt test/expected.txt
+}
 
 function run_case {
     local code="$1"
     local expected=$2
     rm -f $as_file
-    echo  "$code" | go run *.go > $as_file
-    gcc -no-pie $as_file
-    local actual=`./a.out`
+    echo  "$code" | ./${prog_name} > $as_file
+    gcc -no-pie -o out/a.out $as_file
+    local actual=`./out/a.out`
     if [[ "$actual" -eq "$expected" ]];then
         echo "ok"
     else
@@ -19,6 +29,8 @@ function run_case {
     fi
 
 }
+
+run_test_go
 
 run_case "printf(\"hello\")
 printf(\"world\")" "helloworld"
---
+++ test/test.go
@@ -0,0 +1,6 @@
+printf("%d", 2 + 5)
+printf("%d", 2 * 3)
+printf("%d", 3 -2)
+puts("")
+puts("hello")
+puts("world")
