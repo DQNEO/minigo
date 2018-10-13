557eb854b8e05f73dd1f80177fb5a9fe07442526
DQNEO <dqneoo@gmail.com>
2018-10-14 01:17:41 +0900

    improve test

--- t/test.go
+++ t/test.go
@@ -1,12 +1,10 @@
 package main
 
 func fa() {
-	var i int
-	i = 0
-	printf("%d\n", i)
 }
 
 func fb() {
+	printf("%d\n", 0)
 	printf("%d\n", 2 - 1)
 	printf("%d\n", 1 + 1)
 	printf("%d\n", 1 + 1 + 1)
@@ -15,9 +13,16 @@ func fb() {
 	printf("%d\n", 1 + 1 * 5)
 }
 
+func fc() {
+	var i int
+	i = 3
+	printf("%d\n", i + 4)
+}
+
 func main() {
 	fa()
 	fb()
+	fc()
 	puts("hello world")
 }
 
