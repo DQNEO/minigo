19d737a90ef0fbc6bbe319375160bdec81dd986e
DQNEO <dqneoo@gmail.com>
2018-10-15 23:00:34 +0900

    Remove docker container after execution

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # you need docker to run this
-docker run -w /mnt -v `pwd`:/mnt dqneo/ubuntu-build-essential:go ./linux_test.sh
+docker run --rm -w /mnt -v `pwd`:/mnt dqneo/ubuntu-build-essential:go ./linux_test.sh
