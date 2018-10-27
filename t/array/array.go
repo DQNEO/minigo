8cbfaed31ed700fdb78aaf28b1db0d343a372768
DQNEO <dqneoo@gmail.com>
2018-10-28 00:09:29 +0900

    Impl assignment to an element in an array

--- t/array/array.go
+++ t/array/array.go
@@ -2,16 +2,18 @@ package main
 
 import "fmt"
 
-var garray [3]int = [3]int{1,2,3}
+var garray [3]int = [3]int{1,2,0}
 
 func f1() {
+	garray[2] = 3
 	fmt.Printf("%d\n", garray[0])
 	fmt.Printf("%d\n", garray[1])
 	fmt.Printf("%d\n", garray[2])
 }
 
 func f2() {
-	var larray [4]int = [4]int{4,5,6,7}
+	var larray [4]int = [4]int{4,5,0,7}
+	larray[2] = 6
 	fmt.Printf("%d\n", larray[0])
 	fmt.Printf("%d\n", larray[1])
 	fmt.Printf("%d\n", larray[2])
