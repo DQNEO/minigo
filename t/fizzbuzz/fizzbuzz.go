5751132b817c3632039946b45dc128667010cfcc
DQNEO <dqneoo@gmail.com>
2018-11-04 22:51:27 +0900

    abstract begin,end

--- t/fizzbuzz/fizzbuzz.go
+++ t/fizzbuzz/fizzbuzz.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 15; i = i + 1 {
+	var begin = 1
+	var end = 15
+
+	for i := begin; i <= end; i = i + 1 {
 		if i == 3 || i == 6 || i == 9 || i == 12 {
 			fmt.Printf("Fizz\n")
 		} else if i == 5 || i == 10 {
