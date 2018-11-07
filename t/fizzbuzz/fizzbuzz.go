f632c1d75e0cbe1160349ae90de67685644ecd92
DQNEO <dqneoo@gmail.com>
2018-11-07 23:53:05 +0900

    Now we can complie fizzbuzz!!

--- t/fizzbuzz/fizzbuzz.go
+++ t/fizzbuzz/fizzbuzz.go
@@ -6,12 +6,12 @@ func main() {
 	begin, end := 1, 15
 
 	for i := begin; i <= end; i = i + 1 {
-		if i == 3 || i == 6 || i == 9 || i == 12 {
-			fmt.Printf("Fizz\n")
-		} else if i == 5 || i == 10 {
-			fmt.Printf("Buzz\n")
-		} else if i == 15 {
+		if i % 15 == 0 {
 			fmt.Printf("FizzBuzz\n")
+		} else if i % 3 == 0 {
+			fmt.Printf("Fizz\n")
+		} else if i % 5 == 0 {
+			fmt.Printf("Buzz\n")
 		} else {
 			fmt.Printf("%d\n", i)
 		}
