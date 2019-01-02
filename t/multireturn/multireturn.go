ed33b3dca4943719789dcfc467b454b42c66e0fa
DQNEO <dqneoo@gmail.com>
2019-01-02 22:47:48 +0900

    fmt

--- t/multireturn/multireturn.go
+++ t/multireturn/multireturn.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-func multi(a int, b int, c int, d int) (int, int, int,int ) {
-	return b,a,d,c
+func multi(a int, b int, c int, d int) (int, int, int, int) {
+	return b, a, d, c
 }
 
 func main() {
@@ -11,7 +11,7 @@ func main() {
 	var j int = 0
 	var k int = 0
 	var l int = 0
-	i, j,k,l = multi(2,1,4,3)
+	i, j, k, l = multi(2, 1, 4, 3)
 	fmt.Printf("%d\n", i)
 	fmt.Printf("%d\n", j)
 	fmt.Printf("%d\n", k)
