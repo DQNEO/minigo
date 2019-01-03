b7c3546c9a62c2f1261ef01ad07c4f2558f2ee99
DQNEO <dqneoo@gmail.com>
2019-01-03 22:57:33 +0900

    add comment

--- main.go
+++ main.go
@@ -39,6 +39,9 @@ func parseOpts(args []string) {
 
 var internalCode  = `
 const MiniGo int = 1
+
+// println should be a "Predeclared identifiers"
+// https://golang.org/ref/spec#Predeclared_identifiers
 func println(s string) {
 	puts(s)
 }
