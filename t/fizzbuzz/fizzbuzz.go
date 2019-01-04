917a08b1609e98f2e1634646baac001967a2240e
DQNEO <dqneoo@gmail.com>
2019-01-04 09:54:19 +0900

    make fmt.Printf call have always 2 arguments

--- t/fizzbuzz/fizzbuzz.go
+++ t/fizzbuzz/fizzbuzz.go
@@ -7,11 +7,11 @@ func main() {
 
 	for i := begin; i <= end; i++ {
 		if i%15 == 0 {
-			fmt.Printf("FizzBuzz\n")
+			fmt.Printf("%s\n", "FizzBuzz")
 		} else if i%3 == 0 {
-			fmt.Printf("Fizz\n")
+			fmt.Printf("%s\n","Fizz")
 		} else if i%5 == 0 {
-			fmt.Printf("Buzz\n")
+			fmt.Printf("%s\n","Buzz")
 		} else {
 			fmt.Printf("%d\n", i)
 		}
