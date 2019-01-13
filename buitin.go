69b45f7234995ac1e89003adc944a234879bb5cc
DQNEO <dqneoo@gmail.com>
2019-01-13 12:28:31 +0900

    Refactor

--- buitin.go
+++ buitin.go
@@ -1,17 +1,21 @@
 package main
 
+// https://golang.org/ref/spec#Predeclared_identifiers
+
+// Functions:
+//	append cap close complex copy delete imag len
+//	make new panic print println real recover
+
 var builtinCode = `
 package builtin
 
 const MiniGo int = 1
 
-// println should be a "Predeclared identifiers"
-// https://golang.org/ref/spec#Predeclared_identifiers
-func println(s string) {
-	puts(s)
+func len(x interface{}) int {
 }
 
-func len(x interface{}) int {
+func println(s string) {
+	puts(s)
 }
 
 `
