dc3b7f5d7e961d5303ec14ceefca8bef669b6d78
DQNEO <dqneoo@gmail.com>
2019-01-14 20:27:23 +0900

    Separates test funcs

--- t/structpointer/structpointer.go
+++ t/structpointer/structpointer.go
@@ -2,17 +2,7 @@ package main
 
 import "fmt"
 
-func assign() {
-	var u *User
-	u = &User{
-		id:  0,
-		age: 4,
-	}
-	u.age = 5
-	fmt.Printf("%d\n", u.age)
-}
-
-func main() {
+func literal() {
 	var u *User
 	u = &User{
 		id:  1,
@@ -27,6 +17,20 @@ func main() {
 	}
 	fmt.Printf("%d\n", u.id)
 	fmt.Printf("%d\n", u.age)
+}
+
+func assign() {
+	var u *User
+	u = &User{
+		id:  0,
+		age: 4,
+	}
+	u.age = 5
+	fmt.Printf("%d\n", u.age)
+}
+
+func main() {
+	literal()
 	assign()
 }
 
