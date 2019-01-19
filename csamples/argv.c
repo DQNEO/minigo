56686b800701b2ad09420ba0adfd552e2ecad70c
DQNEO <dqneoo@gmail.com>
2019-01-19 22:54:31 +0900

    Sample to store contents of argc and argv to global vars

--- csamples/argv.c
+++ csamples/argv.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+static int global_argc;
+static char **global_argv;
+
 int main(int argc, char **argv) {
     if (argc > 1) {
         printf("argv[1]=%s\n", argv[1]);
     }
+    global_argc = argc;
+    global_argv = argv;
 }
