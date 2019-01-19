3ef77ea1f79bcdce2f084c7fce5d3bd0c0177ab4
DQNEO <dqneoo@gmail.com>
2019-01-19 23:11:24 +0900

    Shows argc as well

--- csamples/argv.c
+++ csamples/argv.c
@@ -5,6 +5,7 @@ static char **global_argv;
 
 int main(int argc, char **argv) {
     if (argc > 1) {
+        printf("argc=%d\n", argc);
         printf("argv[1]=%s\n", argv[1]);
     }
     global_argc = argc;
