e096a28e0493482c8844b20b841fa47e17dfb97b
DQNEO <dqneoo@gmail.com>
2019-01-20 21:54:21 +0900

    Refactor

--- gtype.go
+++ gtype.go
@@ -71,7 +71,8 @@ func (gtype *Gtype) getSize() int {
 		} else if gtype.typ == G_POINTER || gtype.typ == G_STRING || gtype.typ == G_INTERFACE {
 			return ptrSize
 		} else if gtype.typ == G_SLICE {
-			return ptrSize + 8 /* len */ + 8 /* cap */
+			intSize := 8
+			return ptrSize + intSize * 2
 		} else {
 			return gtype.size
 		}
