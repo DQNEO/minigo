04e63a0135d0e6b37786a90246cb03b53d5a1016
DQNEO <dqneoo@gmail.com>
2019-02-17 23:04:23 +0900

    bugfix

--- t/type/type.go
+++ t/type/type.go
@@ -9,11 +9,19 @@ func anytype(x interface{}, y interface{}) {
 	fmt.Printf("%d\n", y)
 }
 
-func main() {
-	var a myint = 1
-	fmt.Printf("%d\n", a)
+func f1() {
+	var a myint = '1'
+	fmt.Printf("%c\n", a)
+}
+
+func f2() {
 	fmt.Printf("%d\n", myarray[1])
 	anytype(3, 4)
+}
+
+func main() {
+	f1()
+	f2()
 }
 
 type myint int
