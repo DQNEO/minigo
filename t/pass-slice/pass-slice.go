055b567fa84ccc8165dcba8999d978060a607e85
DQNEO <dqneoo@gmail.com>
2019-03-13 01:02:58 +0900

    Pass slice

--- t/pass-slice/pass-slice.go
+++ t/pass-slice/pass-slice.go
@@ -3,21 +3,24 @@ package main
 import "fmt"
 
 func handle_slice(s []int) {
-	fmt.Printf("%d\n", s[0])
-	fmt.Printf("%d\n", s[1])
-	fmt.Printf("%d\n", s[2])
-	fmt.Printf("len=%d\n", len(s))
-	//x[0] = 1
-	//return x
+	fmt.Printf("%d\n", len(s) - 2) // 1
+	fmt.Printf("%d\n", cap(s) - 3) // 2
+	fmt.Printf("%d\n", s[0] - 12) // 3
+	fmt.Printf("%d\n", s[1] - 10) // 4
+	s[2] = 3
+	fmt.Printf("%d\n", s[2] + 2) // 5
 }
 
+var array [5]int = [...]int{15,14,13,12,11}
+
 func f1() {
-	var s []int = []int{11,13,15}
-	fmt.Printf("len=%d\n", len(s))
+	var s []int = array[0:3]
 	handle_slice(s)
-	fmt.Printf("%d\n", s[0])
-	fmt.Printf("%d\n", s[1])
-	fmt.Printf("%d\n", s[2])
+	fmt.Printf("%d\n", len(s) + 3) // 6
+	fmt.Printf("%d\n", cap(s) + 2) // 7
+	fmt.Printf("%d\n", s[0] - 7) // 8
+	fmt.Printf("%d\n", s[1] - 5) // 9
+	fmt.Printf("%d\n", s[2] + 7) // 10
 }
 
 func main() {
