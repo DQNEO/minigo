1c626490f472a7ae3f05716df2ad398730390072
DQNEO <dqneoo@gmail.com>
2019-03-17 03:54:13 +0900

    Implement strings.HasSuffix

--- stdlib/strings/strings.go
+++ stdlib/strings/strings.go
@@ -1,6 +1,12 @@
 package strngs
 
 func HasSuffix(s string, suffix string) bool {
+	if len(s) >= len(suffix) {
+		var suf string
+		suf = s[len(s)-len(suffix):]
+		return suf == suffix
+	}
+	return false
 }
 
 func Contains(s string) bool {
---
+++ t/strings/strings.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func f1() {
+	s := "main.go"
+	suffix := ".go"
+	if strings.HasSuffix(s, suffix) {
+		fmt.Printf("1\n")
+	}
+}
+
+
+func main() {
+	f1()
+}
