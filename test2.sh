722ea2fdf10e90d8c37b2e1290156945deee2e5e
DQNEO <dqneoo@gmail.com>
2019-03-17 21:33:51 +0900

    switch by OS

--- test2.sh
+++ test2.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 ./minigo t2/panic/panic.go > out/a.s
-docker run --rm -w /mnt -v `pwd`:/mnt dqneo/ubuntu-build-essential bash -c 'gcc -g -no-pie out/a.s && ./a.out >/dev/null'
+
+if [[ `uname` == "Darwin" ]];then
+    # for MacOS
+    docker run --rm -w /mnt -v `pwd`:/mnt dqneo/ubuntu-build-essential bash -c 'gcc -g -no-pie out/a.s && ./a.out >/dev/null'
+else
+    # for Linux
+    gcc -g -no-pie out/a.s && ./a.out >/dev/null
+fi
+
 if [[ $? -ne 1 ]]; then
     echo "FAILED"
     exit 1
