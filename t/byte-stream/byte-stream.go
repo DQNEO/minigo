a66fc2208e0dc7a10ec516a93ffc9f88afa57b0d
DQNEO <dqneoo@gmail.com>
2019-03-18 11:22:20 +0900

    unused

---
+++ t/byte-stream/byte-stream.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
+
+type ByteStream struct {
+	filename  string
+	source    []byte
+	nextIndex int
+	line      int
+	column    int
+}
+
+func NewByteStreamFromString(name string, contents string) *ByteStream {
+	return &ByteStream{
+		filename:  name,
+		source:    []byte(contents),
+		nextIndex: 0,
+		line:      1,
+		column:    0,
+	}
+}
+
+func NewByteStreamFromFile(path string) *ByteStream {
+	s := readFile(path)
+	return &ByteStream{
+		filename:  path,
+		source:    s,
+		nextIndex: 0,
+		line:      1,
+		column:    0,
+	}
+}
+
+func readFile(filename string) []byte {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
+}
+
+func (bs *ByteStream) location() string {
+	return fmt.Sprintf("%s:%d:%d", bs.filename, bs.line, bs.column)
+}
+
+func (bs *ByteStream) get() (byte, error) {
+	if bs.nextIndex >= len(bs.source) {
+		return 0, errors.New("EOF")
+	}
+	r := bs.source[bs.nextIndex]
+	if r == '\n' {
+		bs.line++
+		bs.column = 1
+	}
+	bs.nextIndex++
+	bs.column++
+	return r, nil
+}
+
+func (bs *ByteStream) unget() {
+	bs.nextIndex--
+	r := bs.source[bs.nextIndex]
+	if r == '\n' {
+		bs.line--
+	}
+}
+
+func f1() {
+	filename := "/etc/hosts"
+	bs := NewByteStreamFromFile(filename)
+	fmt.Printf("%s\n", bs.source)
+}
+
+func main() {
+	f1()
+}
