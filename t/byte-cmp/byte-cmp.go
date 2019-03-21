6e086b4d81180ec03dd97c8df0f73306c127e28a
DQNEO <dqneoo@gmail.com>
2019-03-22 00:38:55 +0900

    Add more tests

--- t/byte-cmp/byte-cmp.go
+++ t/byte-cmp/byte-cmp.go
@@ -1,16 +1,30 @@
 package main
 
 func f1() {
-	var x byte
+	var x byte = 'a'
 	var e byte = 'e'
 
 	if e <= 'z' {
 		println("1")
 	} else {
-		println("NG")
+		println(x)
+	}
+}
+
+func f2() {
+	var c1 byte = 'p'
+	var c2 byte = 'a'
+
+	if 'a' <= c1 && c1 <= 'z' {
+		println("2")
+	}
+
+	if 'a' <= c2 && c2 <= 'z' {
+		println("3")
 	}
 }
 
 func main() {
 	f1()
+	f2()
 }
