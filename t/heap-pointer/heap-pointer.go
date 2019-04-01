cd424745236bf7f313325cf22aa7e96f8760bad4
DQNEO <dqneoo@gmail.com>
2019-04-02 00:27:09 +0900

    tmp

--- t/heap-pointer/heap-pointer.go
+++ t/heap-pointer/heap-pointer.go
@@ -9,24 +9,30 @@ type Hobbit struct {
 
 func return_ptr() *Hobbit {
 	var ptr = &Hobbit{
-		id:1,
-		age:123,
+		id:2,
+		age:3,
 	}
 	return ptr
 }
 
-func dummy() {
+func dummy1() {
 	var ptr = &Hobbit{
 		id:0,
-		age:123,
+		age:4,
 	}
-	fmt.Printf("%d\n", ptr.id)
+	fmt.Printf("%d\n", ptr.id) // 0
 }
 
+func dummy2() {
+	var array [4]int = [...]int{0,0,0,10}
+	fmt.Printf("%d\n", array[3] - 9) // 1
+}
 func f1() {
 	p := return_ptr()
-	dummy()
+	dummy1()
+	dummy2()
 	fmt.Printf("%d\n", p.id)
+	fmt.Printf("%d\n", p.age)
 }
 
 func main() {
