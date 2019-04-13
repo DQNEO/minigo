09a08e97b2aa8c53c103afd49c97453827977360
DQNEO <dqneoo@gmail.com>
2019-04-14 01:41:32 +0900

    refactor

--- t/sprintf/sprintf.go
+++ t/sprintf/sprintf.go
@@ -2,28 +2,32 @@ package main
 
 func doPrintf(format string, a []interface{}) string {
 	var buf [1024]byte
+	var a0 interface{}
+	var a1 interface{}
+	var a2 interface{}
+	var a3 interface{}
 	var numred int
 	switch len(a) {
 	case 0:
 		numred = sprintf(buf, format)
 	case 1:
-			var a0 interface{} = a[0]
+		a0 = a[0]
 		numred = sprintf(buf, format, *a0)
 
 	case 2:
-			var a0 interface{} = a[0]
-			var a1 interface{} = a[1]
+		a0 = a[0]
+		a1 = a[1]
 		numred = sprintf(buf, format, *a0, *a1)
 	case 3:
-			var a0 interface{} = a[0]
-			var a1 interface{} = a[1]
-			var a2 interface{} = a[2]
+		a0 = a[0]
+		a1 = a[1]
+		a2 = a[2]
 		numred = sprintf(buf, format, *a0, *a1, *a2)
 	case 4:
-		var a0 interface{} = a[0]
-		var a1 interface{} = a[1]
-		var a2 interface{} = a[2]
-		var a3 interface{} = a[3]
+		a0 = a[0]
+		a1 = a[1]
+		a2 = a[2]
+		a3 = a[3]
 		numred = sprintf(buf, format, *a0, *a1, *a2, *a3)
 	default:
 		printf("ERROR: doPrintf cannot handle more than 4 params")
@@ -50,7 +54,7 @@ func f0() {
 
 func f1() {
 	var a []interface{}
-	var i int  = 123
+	var i int = 123
 	var ifc interface{}
 	ifc = i
 	a = append(a, ifc)
@@ -60,7 +64,7 @@ func f1() {
 func f2() {
 	var a []interface{}
 	var i int = 123
-	var i2 int  = 456
+	var i2 int = 456
 	var ifc interface{}
 	var ifc2 interface{}
 	ifc = i
@@ -73,8 +77,8 @@ func f2() {
 
 func f3() {
 	var a []interface{}
-	var s string  = "hello"
-	var s2 string  = "world"
+	var s string = "hello"
+	var s2 string = "world"
 	var ifc interface{}
 	var ifc2 interface{}
 	ifc = s
@@ -86,7 +90,7 @@ func f3() {
 
 func f4() {
 	var a []interface{}
-	var s string  = "hello"
+	var s string = "hello"
 	var i int = 123
 	var ifc interface{}
 	var ifc2 interface{}
@@ -99,7 +103,7 @@ func f4() {
 
 func f5() {
 	var a []interface{}
-	var s string  = "hello"
+	var s string = "hello"
 	var i int = 123
 	var i2 int = 456
 	var ifc interface{}
