51a66b15404f711822587ade00835b6d657fc2f4
DQNEO <dqneoo@gmail.com>
2019-04-15 10:50:32 +0900

    Refactor

--- stdlib/os/os.go
+++ stdlib/os/os.go
@@ -1,6 +1,28 @@
 package os
 
-const Stderr = 2
+var Stdout *File
+
+var SStdout = File{
+	id:1,
+}
+
+var Stderr *File
+
+var SStderr = File{
+	id:2,
+}
+
+// File represents an open file descriptor.
+type File struct {
+	id int
+}
+
+func (f *File) Write(b string) (int, error) {
+	var fid int = f.id
+	var n int
+	n = write(fid, b, len(b))
+	return n,nil
+}
 
 var Args []string
 
--- t/os/os.go
+++ t/os/os.go
@@ -1,35 +1,16 @@
 package main
 
-var Stdout *File
-var Stderr *File
+import "os"
 
-var SStdout = File{
-	id:1,
-}
-var SStderr = File{
-	id:2,
-}
-
-// File represents an open file descriptor.
-type File struct {
-	id int
-}
-
-func (f *File) Write(b string) (int, error) {
-	var fid int = f.id
-	var n int
-	n = write(fid, b, len(b))
-	return n,nil
-}
 
 func f1() {
-	var b = "hello stdout\n"
-	Stdout = &SStdout
-	Stdout.Write(b)
+	var a = "hello stdout\n"
+	os.Stdout = &os.SStdout
+	os.Stdout.Write(a)
 
 	var b = "hello stderr\n"
-	Stderr = &SStderr
-	Stderr.Write(b)
+	os.Stderr = &os.SStderr
+	os.Stderr.Write(b)
 
 }
 
