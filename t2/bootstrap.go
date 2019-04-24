0992288f38afded60d468b1af4a0ad47f16660ea
DQNEO <dqneoo@gmail.com>
2019-04-25 02:20:39 +0900

    min file to reproduce the bug

--- t2/bootstrap.go
+++ t2/bootstrap.go
@@ -1,26 +1,10 @@
 package runtime
 
-var runtimeArgc int
-var runtimeArgv *int
-var Args []string //  Do not remove this. Actually this is real os.Args.
-
-var heap [10485760]byte
 var heapTail *int
 
-var heapSlice1 [10485760]byte
-var heapSlice1Tail *byte
-
-var heapSlice8 [10485760]int
-var heapSlice8Tail *int
-
-var heapSlice24 [10485760]interface{}
-var heapSlice24Tail *interface{}
-const intSize = 8
-
-func init() {
-	// set head address of heap
-	heapTail = heap + 0
-	heapSlice1Tail = heapSlice1 + 0
-	heapSlice8Tail = heapSlice8 + 0
-	heapSlice24Tail = heapSlice24 + 0
+func f1(size int) *int {
+	if heapTail + size > 0 {
+		return 0
+	}
 }
+
