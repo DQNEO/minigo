4123eaf35326842d410615ea89e2d5f5697fd773
DQNEO <dqneoo@gmail.com>
2019-05-06 16:03:47 +0900

    Increase heap amount

--- internalcode/runtime.go
+++ internalcode/runtime.go
@@ -4,7 +4,7 @@ var runtimeArgc int
 var runtimeArgv *int
 var Args []string //  Do not remove this. Actually this is real os.Args.
 
-var heap [10485760]byte
+var heap [40485760]byte
 var heapTail *int
 
 const intSize = 8
