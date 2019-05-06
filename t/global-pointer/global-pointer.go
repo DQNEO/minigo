20ee6050865acd0dbb368c31f6e673b1e9fd82d6
DQNEO <dqneoo@gmail.com>
2019-05-06 22:01:23 +0900

    Add test to reproduce the bug

--- t/global-pointer/global-pointer.go
+++ t/global-pointer/global-pointer.go
@@ -13,17 +13,42 @@ type Point struct {
 	y int
 }
 
+
 func f1() {
-	fmt.Printf("%d\n", *GlobalPtr)
+	fmt.Printf("%d\n", *GlobalPtr) // 1
 }
 
 func f2() {
-	fmt.Printf("%d\n", pp1.y)
-	fmt.Printf("%d\n", pp2.x)
+	fmt.Printf("%d\n", pp1.y) // 2
+	fmt.Printf("%d\n", pp2.x) // 3
+}
+
+type Gtype struct {
+	typ int
+	size int
+}
+
+var gInt  = &Gtype{typ: 1, size: 8}
+
+type DeclFunc struct {
+	tok               string
+	pkg               string
+	receiver          *int
+	fname             string
+	rettypes          []*Gtype
+}
+
+var builinLen = &DeclFunc{
+	rettypes: []*Gtype{gInt},
+}
+
+func f3() {
+	fmt.Printf("%d\n", len(builinLen.rettypes) + 3) // 4
 }
 
 func main() {
 	f1()
 	f2()
+	f3()
 }
 
