3cc8b9ee3e92ddffda00176c75e5b690186d5e89
DQNEO <dqneoo@gmail.com>
2019-05-09 21:34:09 +0900

    No need to show the diff on failure

--- unit_test.sh
+++ unit_test.sh
@@ -31,7 +31,7 @@ function as_run {
     # https://stackoverflow.com/questions/33970159/bash-a-out-no-such-file-or-directory-on-running-executable-produced-by-ld
     gcc -no-pie -o $bin_file $obj_file
     $bin_file $ARGS > $actual
-    diff -u $expected $actual
+    diff -uq $expected $actual
 }
 
 function run_unit_test {
