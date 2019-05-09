dd5cbc07dd18ace9e43cd9d2924398b598ae07d9
DQNEO <dqneoo@gmail.com>
2019-05-10 00:21:08 +0900

    Refactor

--- testiterate.sh
+++ testiterate.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 set -u
 
-progname=minigo
-mkdir -p /tmp/$progname
+function iterate() {
+    local progname=$1
+    mkdir -p /tmp/$progname
+    echo iterate by $progname
+    for testfile in t/expected/*.txt
+    do
+        testname=$(basename -s .txt $testfile)
+        echo $progname $testname
+        testtarget=t/$testname/*.go
+        outfile=/tmp/$progname/$testname.token
+        ./$progname --parse-only -d -a $testtarget 2> $outfile
+        if [[ $? -ne 0 ]]; then
+            #cat $outfile
+            echo test $testname failed
+            exit 1
+        fi
+    done
+}
 
-echo iterate by $progname
-for testfile in t/expected/*.txt
-do
-    testname=$(basename -s .txt $testfile)
-    echo $progname $testname
-    testtarget=t/$testname/*.go
-    outfile=/tmp/$progname/$testname.ast
-    ./$progname --parse-only -d -a $testtarget 2> $outfile
-    if [[ $? -ne 0 ]]; then
-        cat $outfile
-        echo test $testname failed
-        exit 1
-    fi
-done
-
-progname=minigo2
-mkdir -p /tmp/$progname
-
-for testfile in t/expected/*.txt
-do
-    testname=$(basename -s .txt $testfile)
-    echo $progname $testname
-    testtarget=t/$testname/*.go
-    outfile=/tmp/$progname/$testname.ast
-    ./$progname --parse-only -d -a $testtarget 2> $outfile
-    if [[ $? -ne 0 ]]; then
-        echo "[FAIL]" $progname $testname
-    fi
-done
-
+iterate minigo
+iterate minigo2
 
 echo "All iteration done"
