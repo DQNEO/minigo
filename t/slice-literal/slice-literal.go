4c7145ee653f44353dd38cd673229396c714fec8
DQNEO <dqneoo@gmail.com>
2019-05-21 21:44:33 +0900

    tmp

--- t/slice-literal/slice-literal.go
+++ t/slice-literal/slice-literal.go
@@ -29,19 +29,12 @@ func f1() {
 		y: 2,
 	}
 	var e Ifc = pp
-	printf("pp=%p\n", pp)
-	dumpInterface(e)
-	printf("*e=%p\n", *e)
 	fmt.Printf("3=%d\n", e.getId()+2) // 3
-	asComment("AAA")
 	var slice []Ifc = []Ifc{e, e, e}
-	asComment("BBB")
-	fmt.Printf("4=%d\n", len(slice)+1)       // 4
+	fmt.Printf("4=%d\n", len(slice)+1) // 4
 	var e2 Ifc
 	e2 = slice[1]
-	dumpInterface(e2)
 	z := e2.getId()
-	printf("z=%d\n", z)
 	fmt.Printf("%d\n", z) // 5
 	return
 }
