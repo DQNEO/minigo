493620c8021bf777c0de376edb05eda2d8e42218
DQNEO <dqneoo@gmail.com>
2019-05-24 22:51:47 +0900

    Separate sema.go

--- main.go
+++ main.go
@@ -223,7 +223,7 @@ func main() {
 	setTypeIds(p.allNamedTypes)
 
 	debugf("resolve done")
-	ir := ast2ir(csl , astFiles, p.stringLiterals, p.allDynamicTypes)
+	ir := makeIR(csl , astFiles, p.stringLiterals, p.allDynamicTypes)
 	ir.emit()
 }
 
@@ -232,94 +232,7 @@ type compiledStdlib struct {
 	uniqImportedPackageNames []string
 }
 
-func setTypeIds(namedTypes []*DeclType) {
-	var typeId = 1 // start with 1 because we want to zero as error
-	for _, concreteNamedType := range namedTypes {
-		concreteNamedType.gtype.receiverTypeId = typeId
-		typeId++
-	}
-}
-
 type stdpkg struct {
 	name  identifier
 	files []*SourceFile
-}
-
-func (ir *IrRoot) composeMethodTable() {
-	var methodTable map[int][]string = map[int][]string{} // receiverTypeId : []methodTable
-	for _, funcdecl := range ir.funcs {
-		if funcdecl.receiver != nil {
-			//debugf("funcdecl:%v", funcdecl)
-			gtype := funcdecl.receiver.getGtype()
-			if gtype.kind == G_POINTER {
-				gtype = gtype.origType
-			}
-			if gtype.relation == nil {
-				errorf("no relation for %#v", funcdecl.receiver.getGtype())
-			}
-			typeId := gtype.relation.gtype.receiverTypeId
-			symbol := funcdecl.getSymbol()
-			methods := methodTable[typeId]
-			methods = append(methods, symbol)
-			methodTable[typeId] = methods
-		}
-	}
-	debugf("set methodTable")
-
-	ir.methodTable = methodTable
-}
-
-func ast2ir(csl *compiledStdlib, files []*SourceFile, stringLiterals []*ExprStringLiteral, allDynamicTypes []*Gtype) *IrRoot {
-	var importedPackages []*stdpkg
-
-	for _, pkgName := range csl.uniqImportedPackageNames {
-		compiledPkg := csl.compiledPackages[identifier(pkgName)]
-		importedPackages = append(importedPackages, compiledPkg)
-	}
-
-	var declvars []*DeclVar
-	var funcs []*DeclFunc
-	for _, pkg := range importedPackages {
-		for _, f := range pkg.files {
-			for _, decl := range f.topLevelDecls {
-				if decl.vardecl != nil {
-					declvars = append(declvars, decl.vardecl)
-				} else if decl.funcdecl != nil {
-					funcs = append(funcs, decl.funcdecl)
-				}
-			}
-		}
-	}
-
-	for _, f := range files {
-		for _, decl := range f.topLevelDecls {
-			if decl.vardecl != nil {
-				declvars = append(declvars, decl.vardecl)
-			} else if decl.funcdecl != nil {
-				funcs = append(funcs, decl.funcdecl)
-			}
-		}
-	}
-
-	root := &IrRoot{}
-
-	root.stringLiterals = stringLiterals // a dirtyworkaround
-	root.vars = declvars
-	root.funcs = funcs
-	root.setDynamicTypes(allDynamicTypes)
-	root.importOS = in_array("os", csl.uniqImportedPackageNames)
-	root.composeMethodTable()
-	return root
-}
-
-func (ir *IrRoot) setDynamicTypes(allDynamicTypes []*Gtype) {
-	var uniquedDTypes []string = builtinTypesAsString
-	for _, gtype := range allDynamicTypes {
-		gs := gtype.String()
-		if !in_array(gs, uniquedDTypes) {
-			uniquedDTypes = append(uniquedDTypes, gs)
-		}
-	}
-
-	ir.uniquedDTypes = uniquedDTypes
 }
