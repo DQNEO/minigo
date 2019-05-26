d66f2ca5be52de0b056b7e5a26ceec304bcbd708
DQNEO <dqneoo@gmail.com>
2019-05-26 22:04:52 +0900

    fix

--- main.go
+++ main.go
@@ -97,6 +97,7 @@ func main() {
 		name:"",
 		files:[]*AstFile{internalUniverse},
 		stringLiterals:p.packageStringLiterals,
+		dynamicTypes:p.packageDynamicTypes,
 	}
 
 	for _, sl := range pUniverse.stringLiterals {
