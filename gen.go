42a77f2afa2b4f733e618ea39deb74ab7c407078
DQNEO <dqneoo@gmail.com>
2019-05-30 10:56:32 +0900

    to upper

--- gen.go
+++ gen.go
@@ -131,14 +131,14 @@ func (f *DeclFunc) emitPrologue() {
 		case G_SLICE, G_INTERFACE, G_MAP:
 			offset -= IntSize * 3
 			param.offset = offset
-			emit("push_arg_%d # third", regIndex+2)
-			emit("push_arg_%d # second", regIndex+1)
-			emit("push_arg_%d # fist \"%s\" %s", regIndex, param.varname, param.getGtype().String())
+			emit("PUSH_ARG_%d # third", regIndex+2)
+			emit("PUSH_ARG_%d # second", regIndex+1)
+			emit("PUSH_ARG_%d # fist \"%s\" %s", regIndex, param.varname, param.getGtype().String())
 			regIndex += sliceWidth
 		default:
 			offset -= IntSize
 			param.offset = offset
-			emit("push_arg_%d # param \"%s\" %s", regIndex, param.varname, param.getGtype().String())
+			emit("PUSH_ARG_%d # param \"%s\" %s", regIndex, param.varname, param.getGtype().String())
 			regIndex += 1
 		}
 	}
