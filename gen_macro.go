005287548069bab27a1d1af596376f0ce0f9c111
DQNEO <dqneoo@gmail.com>
2019-05-30 20:36:26 +0900

    Refactor

--- gen_macro.go
+++ gen_macro.go
@@ -1,139 +1,130 @@
 package main
 
+import "fmt"
+
 func emitMacroDefinitions() {
 	emitWithoutIndent("// MACROS")
 
-	emitWithoutIndent(".macro FUNC_PROLOGUE")
+	macroStart("FUNC_PROLOGUE","")
 	emit("push %%rbp")
 	emit("mov %%rsp, %%rbp")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
 	for i, regi := range RegsForArguments {
-		emitWithoutIndent(".macro POP_TO_ARG_%d", i)
-		emitWithoutIndent("pop %%%s", regi)
-		emitWithoutIndent(".endm")
-		emitNewline()
+		macroStart(fmt.Sprintf("POP_TO_ARG_%d", i), "")
+		emit("pop %%%s", regi)
+		macroEnd()
 	}
 
 	for i, regi := range RegsForArguments {
-		emitWithoutIndent(".macro PUSH_ARG_%d", i)
-		emitWithoutIndent("push %%%s", regi)
-		emitWithoutIndent(".endm")
-		emitNewline()
+		macroStart(fmt.Sprintf("PUSH_ARG_%d", i), "")
+		emit("push %%%s", regi)
+		macroEnd()
 	}
 
-	emitWithoutIndent(".macro PUSH_PRIMITIVE")
+	macroStart("PUSH_PRIMITIVE", "")
 	emit("push %%rax # primitive")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro PUSH_SLICE")
+	macroStart("PUSH_SLICE", "")
 	emit("push %%rax # slice.ptr")
 	emit("push %%rbx # slice.len")
 	emit("push %%rcx # slice.cap")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro PUSH_MAP")
+	macroStart("PUSH_MAP", "")
 	emit("push %%rax # map.ptr")
 	emit("push %%rbx # map.len")
 	emit("push %%rcx # map.cap")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro PUSH_INTERFACE")
+	macroStart("PUSH_INTERFACE", "")
 	emit("push %%rax # ifc.1st")
 	emit("push %%rbx # ifc.2nd")
 	emit("push %%rcx # ifc.3rd")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro POP_SLICE")
+	macroStart("POP_SLICE", "")
 	emit("pop %%rcx # slice.cap")
 	emit("pop %%rbx # slice.len")
 	emit("pop %%rax # slice.ptr")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro POP_MAP")
+	macroStart("POP_MAP", "")
 	emit("pop %%rcx # map.cap")
 	emit("pop %%rbx # map.len")
 	emit("pop %%rax # map.ptr")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro POP_INTERFACE")
+	macroStart("POP_INTERFACE", "")
 	emit("pop %%rcx # ifc.3rd")
 	emit("pop %%rbx # ifc.2nd")
 	emit("pop %%rax # ifc.1st")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro LOAD_EMPTY_SLICE")
+	macroStart("LOAD_EMPTY_SLICE", "")
 	emit("mov $0, %%rax")
 	emit("mov $0, %%rbx")
 	emit("mov $0, %%rcx")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro LOAD_EMPTY_MAP")
+	macroStart("LOAD_EMPTY_MAP", "")
 	emit("mov $0, %%rax")
 	emit("mov $0, %%rbx")
 	emit("mov $0, %%rcx")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro LOAD_EMPTY_INTERFACE")
+	macroStart("LOAD_EMPTY_INTERFACE", "")
 	emit("mov $0, %%rax")
 	emit("mov $0, %%rbx")
 	emit("mov $0, %%rcx")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro LOAD_STRING_LITERAL slabel")
+	macroStart("LOAD_STRING_LITERAL", "slabel")
 	emit("lea \\slabel(%%rip), %%rax")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro LOAD_NUMBER n")
+	macroStart("LOAD_NUMBER",  "n")
 	emit("mov $\\n, %%rax")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro STORE_1_TO_LOCAL offset")
+	macroStart("STORE_1_TO_LOCAL", "offset")
 	emit("mov %%al, \\offset(%%rbp)")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro STORE_8_TO_LOCAL offset")
+	macroStart("STORE_8_TO_LOCAL", "offset")
 	emit("mov %%rax, \\offset(%%rbp)")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro LOAD_8_FROM_LOCAL offset")
+	macroStart("LOAD_8_FROM_LOCAL", "offset")
 	emit("mov \\offset(%%rbp), %%rax")
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro LOAD_SLICE_FROM_LOCAL offset")
+	macroStart("LOAD_SLICE_FROM_LOCAL", "offset")
 	emit("mov \\offset+%2d(%%rbp), %%rax # ptr", 0)
 	emit("mov \\offset+%2d(%%rbp), %%rbx # len", ptrSize)
 	emit("mov \\offset+%2d(%%rbp), %%rcx # cap", ptrSize+IntSize)
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro LOAD_MAP_FROM_LOCAL offset")
+	macroStart("LOAD_MAP_FROM_LOCAL", "offset")
 	emit("mov \\offset+%2d(%%rbp), %%rax # ptr", 0)
 	emit("mov \\offset+%2d(%%rbp), %%rbx # len", ptrSize)
 	emit("mov \\offset+%2d(%%rbp), %%rcx # cap", ptrSize+IntSize)
-	emitWithoutIndent(".endm")
-	emitNewline()
+	macroEnd()
 
-	emitWithoutIndent(".macro LOAD_INTERFACE_FROM_LOCAL offset")
+	macroStart("LOAD_INTERFACE_FROM_LOCAL",  "offset")
 	emit("mov \\offset+%2d(%%rbp), %%rax", 0)
 	emit("mov \\offset+%2d(%%rbp), %%rbx", ptrSize)
 	emit("mov \\offset+%2d(%%rbp), %%rcx", ptrSize+ptrSize)
+	macroEnd()
+}
+
+func macroStart(name string, args string) {
+	emitWithoutIndent(".macro %s %s", name, args)
+}
+
+func macroEnd() {
 	emitWithoutIndent(".endm")
 	emitNewline()
 }
+
