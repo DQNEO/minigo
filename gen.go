1bbb00340eb04b3afed391fbf0ad4ee999dc2dd9
DQNEO <dqneoo@gmail.com>
2019-05-31 22:00:16 +0900

    Refactor

--- gen.go
+++ gen.go
@@ -1447,9 +1447,9 @@ func emitCopyStructFromStack(gtype *Gtype) {
 	//assert(left.getGtype().getSize() == right.getGtype().getSize(), left.token(),"size does not match")
 	emit("pop %%rax") // from
 	emit("pop %%rbx") // to
-	emit("push %%rcx")
+	emit("push %%r15")
 	emit("push %%r11")
-	emit("mov %%rax, %%rcx") // from
+	emit("mov %%rax, %%r15") // from
 	emit("mov %%rbx, %%rax") // to
 
 	emitCopyStructInt(gtype)
@@ -1458,29 +1458,29 @@ func emitCopyStructFromStack(gtype *Gtype) {
 func emitCopyStructInt(gtype *Gtype) {
 	var i int
 	for ; i < gtype.getSize(); i += 8 {
-		emit("movq %d(%%rcx), %%r11", i)
+		emit("movq %d(%%r15), %%r11", i)
 		emit("movq %%r11, %d(%%rax)", i)
 	}
 	for ; i < gtype.getSize(); i += 4 {
-		emit("movl %d(%%rcx), %%r11", i)
+		emit("movl %d(%%r15), %%r11", i)
 		emit("movl %%r11, %d(%%rax)", i)
 	}
 	for ; i < gtype.getSize(); i++ {
-		emit("movb %d(%%rcx), %%r11", i)
+		emit("movb %d(%%r15), %%r11", i)
 		emit("movb %%r11, %d(%%rax)", i)
 	}
 
 	emit("pop %%r11")
-	emit("pop %%rcx")
+	emit("pop %%r15")
 }
 
 // expect rhs address is in the stack top
 func emitCopyStruct(lhs Expr) {
 	//assert(lhs.getGtype().getSize() == right.getGtype().getSize(), lhs.token(),"size does not match")
 	emit("pop %%rax")
-	emit("push %%rcx")
+	emit("push %%r15")
 	emit("push %%r11")
-	emit("mov %%rax, %%rcx")
+	emit("mov %%rax, %%r15")
 	emitAddress(lhs)
 	emitCopyStructInt(lhs.getGtype())
 }
