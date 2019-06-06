cf90b2615de85bbeb00c7b1af901d082da363ed7
DQNEO <dqneoo@gmail.com>
2019-06-06 13:42:48 +0900

    Add fibonacci test

---
+++ t/fibo/fibo.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func fibonacci(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+
+	return fibonacci(n - 1) + fibonacci(n -2)
+}
+
+func main() {
+	var r int
+	r = fibonacci(12)
+	fmt.Printf("%d\n", r)
+}
