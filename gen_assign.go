bcaadc8856560706a65d760e6a3ff654eb5af972
DQNEO <dqneoo@gmail.com>
2019-06-14 20:55:33 +0900

    unwrapRel

--- gen_assign.go
+++ gen_assign.go
@@ -424,6 +424,7 @@ func assignToSlice(lhs Expr, rhs Expr) {
 
 // copy each element
 func assignToArray(lhs Expr, rhs Expr) {
+	rhs = unwrapRel(rhs)
 	emit("# assignToArray")
 	if rel, ok := lhs.(*Relation); ok {
 		lhs = rel.expr
@@ -492,10 +493,8 @@ func assignToArray(lhs Expr, rhs Expr) {
 					val := arrayLiteral.values[i]
 					val.emit()
 				}
-			case *Relation:
-				rel := rhs.(*Relation)
-				arrayVariable, ok := rel.expr.(*ExprVariable)
-				assert(ok, nil, "ok")
+			case *ExprVariable:
+				arrayVariable := rhs.(*ExprVariable)
 				arrayVariable.emitOffsetLoad(elmSize, offsetByIndex)
 			case *ExprStructField:
 				strctField := rhs.(*ExprStructField)
