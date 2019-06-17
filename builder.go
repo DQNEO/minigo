ed75ee8ea9ed3ccf67c5c7bfc77c2c8abe198736
DQNEO <dqneoo@gmail.com>
2019-06-17 21:35:06 +0900

    fix

--- builder.go
+++ builder.go
@@ -143,12 +143,13 @@ func build(universe *AstPackage, iruntime *AstPackage, csl *compiledStdlib, main
 	var packages []*AstPackage
 
 	packages = append(packages, universe)
-	packages = append(packages, iruntime)
 
 	importedPackages := csl.getPackages()
 	for _, pkg := range importedPackages {
 		packages = append(packages, pkg)
 	}
+
+	packages = append(packages, iruntime)
 
 	packages = append(packages, mainPkg)
 
