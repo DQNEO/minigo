0520288091af0f9ff1ad459781b869476e555559
DQNEO <dqneoo@gmail.com>
2019-06-20 22:18:35 +0900

    prepare stirng comparison

--- tokenizer.go
+++ tokenizer.go
@@ -6,6 +6,18 @@ import "fmt"
 type gostring []byte
 type cstring string
 
+func eqGostring2(a gostring, b gostring) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i:=0;i<len(a);i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func eqGostring(a gostring, b gostring) bool {
 	return string(a) == string(b)
 }
