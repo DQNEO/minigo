195e59dfb709d0ddd1b2bfdc3b715f7e3b4e8dd7
DQNEO <dqneoo@gmail.com>
2019-06-20 12:35:40 +0900

    Rename

--- gen_assign.go
+++ gen_assign.go
@@ -363,15 +363,13 @@ func assignToSlice(lhs Expr, rhs Expr) {
 		//
 		// see also https://blog.golang.org/strings
 		conversion := rhs.(*IrExprConversion)
-		targetExpr := unwrapRel(conversion.expr)
+		fromExpr := unwrapRel(conversion.expr)
 		assert(conversion.gtype.getKind() == G_SLICE, rhs.token(), "must be a slice of bytes")
-		assert(targetExpr.getGtype().getKind() == G_STRING, rhs.token(), "must be a string type, but got "+conversion.expr.getGtype().String())
-		//stringVariable, ok := targetExpr.(*ExprVariable)
-	//	assert(ok, rhs.token(), "ok")
-		targetExpr.emit()
+		assert(fromExpr.getGtype().getKind() == G_STRING, rhs.token(), "must be a string type, but got "+conversion.expr.getGtype().String())
+		fromExpr.emit()
 		emit("PUSH_8 # ptr")
 		strlen := &ExprLen{
-			arg: targetExpr,
+			arg: fromExpr,
 		}
 		strlen.emit()
 		emit("PUSH_8 # len")
