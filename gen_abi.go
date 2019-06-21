f8f278189f10b842fadc77ffca3e3ec28ca3e76f
DQNEO <dqneoo@gmail.com>
2019-06-21 23:54:46 +0900

    transpose

--- gen_abi.go
+++ gen_abi.go
@@ -16,10 +16,36 @@ package main
 */
 
 var retRegi [14]string = [14]string{
-	"rax", "rbx", "rcx", "rdx", "rdi", "rsi", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15",
+	"rax",
+	"rbx",
+	"rcx",
+	"rdx",
+	"rdi",
+	"rsi",
+	"r8",
+	"r9",
+	"r10",
+	"r11",
+	"r12",
+	"r13",
+	"r14",
+	"r15",
 }
 
-var RegsForArguments [12]string = [12]string{"rdi", "rsi", "rdx", "rcx", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"}
+var RegsForArguments [12]string = [12]string{
+	"rdi",
+	"rsi",
+	"rdx",
+	"rcx",
+	"r8",
+	"r9",
+	"r10",
+	"r11",
+	"r12",
+	"r13",
+	"r14",
+	"r15",
+}
 
 func (f *DeclFunc) prepare() Emitter {
 
