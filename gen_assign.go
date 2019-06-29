fcd719ec6d1f9040ac2806ed250a2071299956bb
DQNEO <dqneoo@gmail.com>
2019-06-29 22:54:05 +0900

    more emit2

--- gen_assign.go
+++ gen_assign.go
@@ -120,9 +120,9 @@ func emitAssignOneRightToMultiLeft(ast *StmtAssignment) {
 	//emit("# Assign %T %s = %T %s", left, gtype.String(), right, right.getGtype())
 	if leftsMayBeTwo && len(ast.lefts) == 2 {
 		okVariable := ast.lefts[1]
-		emit("# lefts[0] type = %s", ast.lefts[0].getGtype().String())
+		//emit("# lefts[0] type = %s", ast.lefts[0].getGtype().String())
 		okRegister := mapOkRegister(ast.lefts[0].getGtype().is24WidthType())
-		emit("mov %%%s, %%rax # emit okValue", okRegister)
+		emit2("mov %%%s, %%rax # emit okValue", gostring(okRegister))
 		emitSavePrimitive(okVariable)
 	}
 }
