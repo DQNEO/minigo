cd9e479a9381ff14045d350b3ada0f9c1e07fadf
DQNEO <dqneoo@gmail.com>
2019-07-03 00:41:01 +0900

    Eliminate string conversion

--- gen_load.go
+++ gen_load.go
@@ -130,7 +130,7 @@ func (rel *Relation) emit() {
 func (ast *ExprConstVariable) emit() {
 	emit("# *ExprConstVariable.emit() name=%s iotaindex=%d", gostring(ast.name), ast.iotaIndex)
 	assert(ast.iotaIndex < 10000, ast.token(), "iotaindex is too large")
-	assert(ast.val != nil, ast.token(), "const.val for should not be nil:"+string(ast.name))
+	assert(ast.val != nil, ast.token(), "const.val for should not be nil:%s", gostring(ast.name))
 	if ast.hasIotaValue() {
 		emit("# const is iota")
 		val := &ExprNumberLiteral{
