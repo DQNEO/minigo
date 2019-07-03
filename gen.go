3a5e67eed06b0e1f12d30016548b866c9ca8a213
DQNEO <dqneoo@gmail.com>
2019-07-03 09:42:31 +0900

    Eliminate string literals

--- gen.go
+++ gen.go
@@ -158,17 +158,17 @@ func makeLabel() gostring {
 }
 
 func (ast *StmtInc) emit() {
-	emitIncrDecl("ADD_NUMBER 1", ast.operand)
+	emitIncrDecl(S("ADD_NUMBER 1"), ast.operand)
 }
 func (ast *StmtDec) emit() {
-	emitIncrDecl("SUB_NUMBER 1", ast.operand)
+	emitIncrDecl(S("SUB_NUMBER 1"), ast.operand)
 }
 
 // https://golang.org/ref/spec#IncDecStmt
 // As with an assignment, the operand must be addressable or a map index expression.
-func emitIncrDecl(inst string, operand Expr) {
+func emitIncrDecl(inst gostring, operand Expr) {
 	operand.emit()
-	emit(inst)
+	emit(string(inst))
 
 	left := operand
 	emitSavePrimitive(left)
