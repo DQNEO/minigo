f4181ff72023b0bc695bcae9e245280b78c27283
DQNEO <dqneoo@gmail.com>
2019-07-09 06:07:09 -0700

    more gostring

--- t/vaarg/vaarg.go
+++ t/vaarg/vaarg.go
@@ -1,36 +1,36 @@
 package main
 
 
-func receiveVarg(s string, a ...interface{}) {
+func receiveVarg(s gostring, a ...interface{}) {
 	fmtPrintf(S("-\n"))
 	fmtPrintf(S("%d\n"), len(a))
 	fmtPrintf(S("%s\n"), a[0])
 	fmtPrintf(S("%d\n"), a[1])
 }
 
-func receiveIfcSlice(s string, a []interface{}) {
+func receiveIfcSlice(s gostring, a []interface{}) {
 	fmtPrintf(S("-\n"))
 	fmtPrintf(S("%d\n"), len(a))
 	fmtPrintf(S("%s\n"), a[0])
 	fmtPrintf(S("%d\n"), a[1])
 }
 
-var format string = "format-%s-%d\n"
+var format gostring = gostring("format-%s-%d\n")
 
 func f1() {
-	receiveVarg(format, "hello", 123)
+	receiveVarg(format, S("hello"), 123)
 }
 
 func f2() {
 	var a []interface{}
-	a = append(a, "hello")
+	a = append(a, S("hello"))
 	a = append(a, 123)
 	receiveIfcSlice(format, a)
 }
 
 func f3() {
 	var a []interface{}
-	a = append(a, "hello")
+	a = append(a, S("hello"))
 	a = append(a, 123)
 	receiveVarg(format, a...)
 }
