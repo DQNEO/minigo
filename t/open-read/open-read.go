a2456dbb2a2054a4cfdc83dc45ef2c4e970b4bce
DQNEO <dqneoo@gmail.com>
2019-07-13 12:24:01 -0700

    Do not pass string type to libc as it is because it can be a slice of bytes

--- t/open-read/open-read.go
+++ t/open-read/open-read.go
@@ -13,7 +13,8 @@ func main() {
 	var fd int
 
 	fname := os.Args[1]
-	fd = open(fname, O_RDONLY)
+	var filename *byte = fname
+	fd = open(filename, O_RDONLY)
 	ln := read(fd, buf, MYBUFSIZ)
 	s := buf[0:ln]
 	fmtPrintf(S("%s"), s)
