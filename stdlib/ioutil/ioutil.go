760b181fd121954f1b67c197be0592f7aede3b4e
DQNEO <dqneoo@gmail.com>
2019-07-15 09:32:26 -0700

    Rename

--- stdlib/ioutil/ioutil.go
+++ stdlib/ioutil/ioutil.go
@@ -21,12 +21,12 @@ func readAll(f *os.File, capacity int) ([]byte, error) {
 	return b.Bytes() ,err
 }
 
-func ReadFile(filenameAsString string) ([]byte, error) {
+func ReadFile(filename string) ([]byte, error) {
 	var err error
 
 	// Currently, there is no way to declare type of other package, so Let it infer
 	var f *os.File
-	f, err = os.Open(filenameAsString)
+	f, err = os.Open(filename)
 	var n int = MYBUFSIZ
 
 	var buf []byte
