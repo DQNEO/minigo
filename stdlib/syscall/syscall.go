0f90da44a9bb60bbbc8476a19ceedd9ddd4da0cf
DQNEO <dqneoo@gmail.com>
2019-07-14 20:52:05 -0700

    BytePtrFromString

--- stdlib/syscall/syscall.go
+++ stdlib/syscall/syscall.go
@@ -1,8 +1,13 @@
 package syscall
 
+func BytePtrFromString(s string) *byte {
+	var r *byte = s
+	return r
+}
+
 func Open(name string, flag int, perm int) (int, error) {
 	var fd int
-	var pchar *byte = name
+	var pchar *byte = BytePtrFromString(name)
 	fd = open(pchar, flag)
 	return fd, nil
 }
