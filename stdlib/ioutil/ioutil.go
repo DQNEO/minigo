c614561855e2ff8d400772265268258a7913d7b8
DQNEO <dqneoo@gmail.com>
2019-07-14 22:27:41 -0700

    Extract readAll()

--- stdlib/ioutil/ioutil.go
+++ stdlib/ioutil/ioutil.go
@@ -4,21 +4,26 @@ import "os"
 
 const MYBUFSIZ = 65536 * 2
 
-func ReadFile(filenameAsString string) ([]byte, error) {
+func readAll(f *os.File, n int) ([]byte, error) {
 	var buf []byte
-	buf = makeSlice(MYBUFSIZ, MYBUFSIZ, 24)
+	buf = makeSlice(n, n, 24)
+	fid := f.innerFile.fd.Sysfd
+	var nread int
+	nread = read(fid, buf, n)
+	var buf2 []byte
+	buf2 = buf[0:nread:nread]
+	return buf2,nil
+}
+
+func ReadFile(filenameAsString string) ([]byte, error) {
 	var err error
 
 	// Currently, there is no way to declare type of other package, so Let it infer
 	var f *os.File
 	f, err = os.Open(filenameAsString)
-
 	var n int = MYBUFSIZ
 
-	var nread int
-	fid := f.innerFile.fd.Sysfd
-	nread = read(fid, buf, n)
-	var buf2 []byte
-	buf2 = buf[0:nread:nread]
-	return buf2,nil
+	var buf []byte
+	buf, err = readAll(f, n)
+	return buf, err
 }
