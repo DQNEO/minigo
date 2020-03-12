cfce75b611c0c41ce788fcf09215a9fa7205eaa0
DQNEO <dqneoo@gmail.com>
2020-03-12 18:55:45 +0900

    Follow official go design

--- internal/runtime/asm_amd64.s
+++ internal/runtime/asm_amd64.s
@@ -4,17 +4,21 @@
 _start:
   mov 0(%rsp), %rdx # argc
   lea 8(%rsp), %rsi # argv
-  jmp runtime.rt0_go
+  jmp iruntime.rt0_go
 
-runtime.rt0_go:
+iruntime.rt0_go:
+  call iruntime.args
+  mov $0, %rax
+  mov $0, %rbx
+  jmp _init_packages
+
+iruntime.args:
   mov %rdx, %rax
   mov %rsi, %rbx
   mov %rbx, iruntime.argv+0(%rip)  # ptr
   mov %rax, iruntime.argv+8(%rip)  # len
   mov %rax, iruntime.argv+16(%rip) # cap
-  mov $0, %rax
-  mov $0, %rbx
-  jmp _init_packages
+  ret
 
 iruntime.main:
   FUNCALL main.main
