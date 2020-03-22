7f636c8364706f54f11f048f10d4caa6e0b87a00
DQNEO <dqneoo@gmail.com>
2020-03-23 07:26:27 +0900

    Add q

--- internal/runtime/asm_amd64.s
+++ internal/runtime/asm_amd64.s
@@ -2,33 +2,33 @@
 .text
   .global _start
 _start:
-  mov %rsp,    %rbp # initial stack top addr
-  mov 0(%rsp), %rdx # argc
-  lea 8(%rsp), %rsi # argv
+  movq %rsp,    %rbp # initial stack top addr
+  movq 0(%rsp), %rdx # argc
+  leaq 8(%rsp), %rsi # argv
   # get envp
-  mov %rbp, %rax # stack top addr
-  mov %rdx, %rbx # argc
-  imul $8,  %rbx # argc * 8
-  add %rbx, %rax # stack top addr + (argc * 8)
-  add $16,  %rax # + 16 (skip null and go to next) => envp
-  mov %rax, iruntime.envp+0(%rip) # envp
-  mov $0, %rax
-  mov $0, %rbx
+  movq %rbp, %rax # stack top addr
+  movq %rdx, %rbx # argc
+  imulq $8,  %rbx # argc * 8
+  addq %rbx, %rax # stack top addr + (argc * 8)
+  addq $16,  %rax # + 16 (skip null and go to next) => envp
+  movq %rax, iruntime.envp+0(%rip) # envp
+  movq $0, %rax
+  movq $0, %rbx
   jmp iruntime.rt0_go
 
 iruntime.rt0_go:
-  call iruntime.args
+  callq iruntime.args
   jmp _init_packages
 
 iruntime.args:
-  mov %rdx, %rax
-  mov %rsi, %rbx
-  mov %rbx, iruntime.argv+0(%rip)  # ptr
-  mov %rax, iruntime.argv+8(%rip)  # len
-  mov %rax, iruntime.argv+16(%rip) # cap
+  movq %rdx, %rax
+  movq %rsi, %rbx
+  movq %rbx, iruntime.argv+0(%rip)  # ptr
+  movq %rax, iruntime.argv+8(%rip)  # len
+  movq %rax, iruntime.argv+16(%rip) # cap
   ret
 
 iruntime.main:
   FUNCALL main.main
-  mov $0,  %rdi
+  movq $0,  %rdi
   FUNCALL iruntime.exit
