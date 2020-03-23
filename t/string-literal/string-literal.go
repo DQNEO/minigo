6a0dc5125828cb6fc19e0c941058a73344f9189d
DQNEO <dqneoo@gmail.com>
2020-03-23 13:25:20 +0900

    add q suffix

--- t/string-literal/string-literal.go
+++ t/string-literal/string-literal.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func f1() {
-	var format string = "lea \\varname+\\offset(%%rip), %%rax"
+	var format string = "leaq \\varname+\\offset(%%rip), %%rax"
 	s := fmt.Sprintf(format)
 	fmt.Println(s)
 }
