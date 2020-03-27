b3879b83a8f82b9ffcd5e0f609c1d1532ccfc819
DQNEO <dqneoo@gmail.com>
2020-03-27 22:43:35 +0900

    Func decl without body is ok here

--- internal/runtime/ignore.go
+++ internal/runtime/ignore.go
@@ -1,6 +1,4 @@
 // Declarations only. Minigo won't parse this file.
 package runtime
 
-func Syscall(num uintptr, u uintptr, u2 uintptr, u3 uintptr) uintptr {
-	return 0
-}
+func Syscall(num uintptr, u uintptr, u2 uintptr, u3 uintptr) uintptr
